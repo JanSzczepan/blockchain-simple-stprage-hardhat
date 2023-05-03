9fc1bb397f4b0b1af47aab9015426689a1f49feb
Jan Szczepan <janszczepan62@gmail.com>
2023-05-03 17:46:35 +0200

    Gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import './tasks/block-number'
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ''
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
 
 const config: HardhatUserConfig = {
    defaultNetwork: 'hardhat',
@@ -23,6 +24,14 @@ const config: HardhatUserConfig = {
    solidity: '0.8.18',
    etherscan: {
       apiKey: ETHERSCAN_API_KEY,
+   },
+   gasReporter: {
+      enabled: true,
+      currency: 'USD',
+      outputFile: 'gas-report.txt',
+      noColors: true,
+      coinmarketcap: COINMARKETCAP_API_KEY,
+      // token: ''
    },
 }
 
