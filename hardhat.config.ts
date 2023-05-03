d00ab3e6cf7db5bbb9e47685c1ff3a69b1471a4a
Jan Szczepan <janszczepan62@gmail.com>
2023-05-03 17:04:40 +0200

    Verify contract on Etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ''
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 
 const config: HardhatUserConfig = {
    defaultNetwork: 'hardhat',
@@ -19,6 +20,9 @@ const config: HardhatUserConfig = {
       },
    },
    solidity: '0.8.18',
+   etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+   },
 }
 
 export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'hardhat'
+import { ethers, run, network } from 'hardhat'
 
 async function main() {
    const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
@@ -6,7 +6,38 @@ async function main() {
    const simpleStorage = await simpleStorageFactory.deploy()
    await simpleStorage.deployed()
 
+   if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+      await simpleStorage.deployTransaction.wait(6)
+      await verify(simpleStorage.address, [])
+   }
+
    console.log('SimpleStorage deployed to:', simpleStorage.address)
+
+   let currentValue = await simpleStorage.retrieve()
+   console.log(`Current value: ${currentValue}`)
+
+   console.log('Updating contract...')
+   const transactionResponse = await simpleStorage.store(6)
+   await transactionResponse.wait()
+   currentValue = await simpleStorage.retrieve()
+   console.log(`Current value: ${currentValue}`)
+}
+
+async function verify(contractAddress: string, args: any[]) {
+   console.log('Veryfying contract...')
+
+   try {
+      await run('verify:verify', {
+         address: contractAddress,
+         constructorArguments: args,
+      })
+   } catch (error: any) {
+      if (error.message.toLowerCase().includes('already verified')) {
+         console.log('Already verified!')
+      } else {
+         console.log(error)
+      }
+   }
 }
 
 main()
